c50628b00f9f4121076b11930932a6d9e7a779ef
Enrico Olivieri <enrico.olivieri@live.it>
2020-10-22 16:58:01 +0200

    dadi complete

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-var emails = ['enrico.olivieri1@live.it', 'enrico.olivieri2@live.it', 'enrico.olivieri3@live.it', 'enrico.olivieri4@live.it', 'enrico.olivieri5@live.it']
+//esercizio mail
+// var emails = ['enrico.olivieri1@live.it', 'enrico.olivieri2@live.it', 'enrico.olivieri3@live.it', 'enrico.olivieri4@live.it', 'enrico.olivieri5@live.it']
+//
+// var inputEmail = prompt('Inserire indirizzo mail.')
+//
+// var advise = 'Mail non trovata.';
+//
+// for (var i = 0; i < emails.length; i++){
+//   if(emails == inputEmail){
+//     advise = 'Mail trovata';
+//   }
+// }
 
-var inputEmail = prompt('Inserire indirizzo mail.')
+//console.log(advise);
+//document.getElementById('advise').innerHTML = advise;
 
-var advise = 'Mail non trovata.';
+//esercizio dadi
+var player = Math.floor(Math.random() * 6) + 1;
 
-for (var i = 0; i < emails.length; i++){
-  if(emails == inputEmail){
-    advise = 'Mail trovata';
-  }
+console.log(player);
+
+var pc = Math.floor(Math.random() * 6) + 1;
+
+console.log(pc);
+
+if (player > pc){
+  console.log('Complimenti hai vinto');
+} else if (pc > player){
+  console.log('Hai perso');
+} else {
+  console.log('Parità');
 }
-
-console.log(advise);
-document.getElementById('advise').innerHTML = advise;
